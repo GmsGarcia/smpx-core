7128cc0d05b4994500430c34a33e9cae81df074d
GmsGarcia <gmsgarcia18@gmail.com>
2025-12-02 11:38:10 +0000

    fix(commands): change SmpxCommand.suggestPlayerNames() return type to List
    It used to cast Bukkit.getOnlinePlayers() to an ArrayList causing an exception.

--- src/main/java/pt/gmsgarcia/smpx/core/commands/SmpxCommand.java
+++ src/main/java/pt/gmsgarcia/smpx/core/commands/SmpxCommand.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import pt.gmsgarcia.smpx.core.SmpxCore;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public abstract class SmpxCommand implements BasicCommand {
@@ -28,17 +29,17 @@ public abstract class SmpxCommand implements BasicCommand {
         return true;
     }
 
-    protected ArrayList<String> suggestPlayerNames(CommandSender sender, String permission, String @NotNull [] args, int pos) {
+    protected List<String> suggestPlayerNames(CommandSender sender, String permission, String @NotNull [] args, int pos) {
         if (!hasPermission(sender, permission)) new ArrayList<>();
 
         if (args.length == 0) {
-            return (ArrayList<String>) Bukkit.getOnlinePlayers().stream()
+            return Bukkit.getOnlinePlayers().stream()
                     .map(Player::getName)
                     .toList();
         }
 
         if (args.length == 1 && (pos >= 0 && pos < args.length)) {
-            return (ArrayList<String>) Bukkit.getOnlinePlayers().stream()
+            return Bukkit.getOnlinePlayers().stream()
                     .map(Player::getName)
                     .filter(name -> name.toLowerCase(Locale.ROOT).startsWith(args[pos].toLowerCase(Locale.ROOT)))
                     .toList();
